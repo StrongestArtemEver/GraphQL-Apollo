774b99b99cf9fbdb8eb2dc93720849510be6e719
Fadin Artem <a.fadin-forwork@mail.ru>
2024-08-02 07:51:45 +0500

    add: form

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       
+       <form>
+        <input type='text'></input>
+        <input type='number'></input>
+        <div className='btns'>
+          <button>Создать</button>
+          <button>Получить</button>
+        </div>
+       </form>
     </div>
   );
 }
