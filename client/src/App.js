16429ab35649081a239c1a8981bb64bd056f4eef
Fadin Artem <a.fadin-forwork@mail.ru>
2024-08-02 08:00:20 +0500

    add : users element

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
   return (
     <div className="App">
-       <form>
-        <input type='text'></input>
-        <input type='number'></input>
-        <div className='btns'>
+      <form>
+        <input type="text"></input>
+        <input type="number"></input>
+        <div className="btns">
           <button>Создать</button>
           <button>Получить</button>
         </div>
-       </form>
+      </form>
+      {users.map((user) => {
+        return (
+          <div className="user">
+            {user.id}. 
+            {user.username}
+            {user.age}
+          </div>
+        );
+      })}
     </div>
   );
 }
